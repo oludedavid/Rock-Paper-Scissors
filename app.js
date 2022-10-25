4ee08d18ac565b85d8468f22a1ffa25510d4d44b
Olude david <odavidolumide@gmail.com>
2022-10-25 07:31:46 +0200

    rps

--- app.js
+++ app.js
@@ -1,113 +1,118 @@
-// //create a function that returns a random string "Rock", "Paper", "Scissors"
-// const computerPlay = () => {
-//   //Create an array of strings
-//   let arryOfStrings = ["rock", "paper", "scissors"];
+//get the buttons
+let rock = document.querySelector(".rock");
+let paper = document.querySelector(".paper");
+let scissors = document.querySelector(".scissors");
+let reset = document.querySelector(".reset");
+let playerWinCount = 0;
+let computerWinCount = 0;
+let playerWinText = document.querySelector(".player-wins-count");
+let compWinText = document.querySelector(".computer-wins-count");
 
-//   //Get array length
-//   let lengthOfArray = arryOfStrings.length;
+playerWinText.textContent = 0;
+compWinText.textContent = 0;
 
-//   //set the limit of the random variable to the length of the array
-//   let randNum = Math.random() * lengthOfArray;
+// create a function that returns a random string "Rock", "Paper", "Scissors"
+const computerPlay = () => {
+  //Create an array of strings
+  const arryOfStrings = ["rock", "paper", "scissors"];
 
-//   //round it down to get an Integer
-//   let roundedRandNum = Math.floor(randNum);
+  //Get array length
+  let lengthOfArray = arryOfStrings.length;
 
-//   //In order the values in the array randomly we use the round num as our key
-//   let randomStrings = arryOfStrings[roundedRandNum];
-//   return randomStrings;
-// };
+  //set the limit of the random variable to the length of the array
+  let randNum = Math.random() * lengthOfArray;
+
+  //round it down to get an Integer
+  let roundedRandNum = Math.floor(randNum);
+
+  //In order the values in the array randomly we use the round num as our key
+  let randomStrings = arryOfStrings[roundedRandNum];
+  return randomStrings;
+};
 
 // //A function that plays a single round of RPS.
-// //A rock beats scissors, scissors beat paper by cutting it, and paper beats rock by covering it.
-// const playSingleRound = (playerSelection, computerSelection) => {
-//   //player wins = 0, computer wins= 1
-//   let winner = null;
-//   //get players input
-//   let playerInputInLowerCase = playerSelection.toLowerCase();
+//A rock beats scissors, scissors beat paper by cutting it, and paper beats rock by covering it.
+const playSingleRound = (playerSelection, computerSelection) => {
+  //player wins = 0, computer wins= 1
+  let winner = null;
+  //get players input
+  let playerInputInLowerCase = playerSelection.toLowerCase();
 
-//   //Validate players input
-//   //check if the user inputs the right input
-//   //   if (playerInputInLowerCase != "rock" && "paper" | "scissors") {
-//   //     alert("Please choose only either 'Rock', 'Paper, 'Scissors'");
-//   //   }
+  let result = document.querySelector(".result");
 
-//   //If players input is correct then return the winner
-//   if (playerInputInLowerCase == "rock" || "paper" || "scissors") {
-//     if (computerSelection === "rock" && playerInputInLowerCase === "scissors") {
-//       alert("You Lose Rock beats Scissors");
-//       winner = 1;
-//     } else if (
-//       computerSelection === "rock" &&
-//       playerInputInLowerCase === "paper"
-//     ) {
-//       alert("You Win Paper beats Rock");
-//       winner = 0;
-//     } else if (
-//       computerSelection === "rock" &&
-//       playerInputInLowerCase === "rock"
-//     ) {
-//       alert("no  winner");
-//       winner = "no winner";
-//     } else if (
-//       computerSelection === "paper" &&
-//       playerInputInLowerCase === "rock"
-//     ) {
-//       alert("You Lose Paper beats Rock");
-//       winner = 1;
-//     } else if (
-//       computerSelection === "paper" &&
-//       playerInputInLowerCase === "scissors"
-//     ) {
-//       alert("You Win Scissors beats Paper");
-//       winner = 0;
-//     } else if (
-//       computerSelection === "paper" &&
-//       playerInputInLowerCase === "paper"
-//     ) {
-//       alert("no winner");
-//       winner = "no winner";
-//     } else if (
-//       computerSelection === "scissors" &&
-//       playerInputInLowerCase === "paper"
-//     ) {
-//       alert("You Lose Scissors beats Paper");
-//       winner = 1;
-//     } else if (
-//       computerSelection === "scissors" &&
-//       playerInputInLowerCase === "rock"
-//     ) {
-//       alert("You Win Rock beats Paper");
-//       winner = 0;
-//     } else if (
-//       computerSelection === "scissors" &&
-//       playerInputInLowerCase === "scissors"
-//     ) {
-//       alert("no winner");
-//       winner = "no winner";
-//     }
-//   } else {
-//     alert("Please insert a Value!");
-//   }
+  //If players input is correct then return the winner
+  if (playerInputInLowerCase == "rock" || "paper" || "scissors") {
+    if (computerSelection === "rock" && playerInputInLowerCase === "scissors") {
+      result.textContent = "You Lose Rock beats Scissors";
+      winner = 1;
+      compWinText.textContent = computerWinCount++;
+    } else if (
+      computerSelection === "rock" &&
+      playerInputInLowerCase === "paper"
+    ) {
+      result.textContent = "You Win Paper beats Rock";
+      winner = 0;
+      playerWinText.textContent = playerWinCount++;
+    } else if (
+      computerSelection === "rock" &&
+      playerInputInLowerCase === "rock"
+    ) {
+      result.textContent = "no  winner";
+      winner = "no winner";
+    } else if (
+      computerSelection === "paper" &&
+      playerInputInLowerCase === "rock"
+    ) {
+      result.textContent = "You Lose Paper beats Rock";
+      winner = 1;
+      compWinText.textContent = computerWinCount++;
+    } else if (
+      computerSelection === "paper" &&
+      playerInputInLowerCase === "scissors"
+    ) {
+      result.textContent = "You Win Scissors beats Paper";
+      winner = 0;
+      playerWinText.textContent = playerWinCount++;
+    } else if (
+      computerSelection === "paper" &&
+      playerInputInLowerCase === "paper"
+    ) {
+      result.textContent = "no winner";
+      winner = "no winner";
+    } else if (
+      computerSelection === "scissors" &&
+      playerInputInLowerCase === "paper"
+    ) {
+      result.textContent = "You Lose Scissors beats Paper";
+      winner = 1;
+      compWinText.textContent = computerWinCount++;
+    } else if (
+      computerSelection === "scissors" &&
+      playerInputInLowerCase === "rock"
+    ) {
+      result.textContent = "You Win Rock beats Paper";
+      winner = 0;
+      playerWinText.textContent = playerWinCount++;
+    } else if (
+      computerSelection === "scissors" &&
+      playerInputInLowerCase === "scissors"
+    ) {
+      result.textContent = "no winner";
+      winner = "no winner";
+    }
+  } else {
+    result.textContent = "Please make a Selection!";
+  }
 
-//   return winner;
-// };
-// // const playerSelection = "rock";
-// // const computerSelection = computerPlay();
-// // playSingleRound(playerSelection, computerSelection)
-// const game = () => {
-//   let computerSelection = computerPlay();
-//   let round = 5;
-//   for (let i = 0; i < round; i++) {
-//     let playerSelection = prompt(
-//       "Please Choose a Value between: 'Rock', 'Paper', and 'Scissors' "
-//     );
+  return winner;
+};
 
-//     playSingleRound(playerSelection, computerSelection);
-//   }
-// };
-
-// window.addEventListener("load", () => {
-//   alert("Welcome!");
-//   alert("Lets play a five round game of Rock, Paper and Scissors!");
-//   game();
-// });
+rock.addEventListener("click", function () {
+  playSingleRound("rock", computerPlay());
+});
+paper.addEventListener("click", function () {
+  playSingleRound("paper", computerPlay());
+});
+scissors.addEventListener("click", function () {
+  playSingleRound("scissors", computerPlay());
+});
